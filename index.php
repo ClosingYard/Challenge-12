23f3d6c131b1017efe35bcc58d1d11866370c465
TheCodingBearbarian <96289130+TheCodingBearbarian@users.noreply.github.com>
2022-06-13 13:30:35 +0200

    commit

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
+<?php
+    require_once("database.inc.php");
+    $sql = "SELECT * FROM vraag";
+    $result = getData($sql);
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tempELOELOE</title>
-    <link rel="stylesheet" href="index.css">
+    <title>30seconds groep9</title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
+    <?php
+        foreach($result as $key=>$value) {
+            print_r($value);
+        }
+    ?>
+
+
+
     <div class="header">30 Secondes</div>
     
     <div class="vragen">
